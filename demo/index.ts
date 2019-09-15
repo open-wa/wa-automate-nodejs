fbeb6442139c0d7d8e958527513385c34bc6dacd
shah@idk.uno <1/gBagpyQy5Kj-oJvoEIB67DBqDWhUWcJ9YXFYa3Cxus3UmAcRDNdli979D_ZOE20U>
2019-09-16 00:31:41 +0100

    small fix

--- demo/index.ts
+++ demo/index.ts
@@ -2,24 +2,28 @@
 
 // var create = require("sulla").create;
 // import { create, Whatsapp, decryptMedia} from 'sulla-hotfix';
-import { create, Whatsapp, decryptMedia} from '../dist/index';
-const mime = require('mime-types')
+import { create, Whatsapp, decryptMedia } from '../dist/index';
+const mime = require('mime-types');
 const fs = require('fs');
 
 function start(client: Whatsapp) {
   client.onMessage(async message => {
-  console.log('TCL: start -> message', message);
-  //@ts-ignore
-  if(message.mimetype) {
-    const mediaData =  await decryptMedia(message);
-  //@ts-ignore
-    fs.writeFile(`${message.t}.${mime.extension(message.mimetype)}`, mediaData, function (err) {
-      if (err) {
-          return console.log(err);
-      }
-      console.log("The file was saved!");
-  });
-  }
+    console.log('TCL: start -> message', message);
+    //@ts-ignore
+    if (message.mimetype) {
+      const mediaData = await decryptMedia(message);
+      fs.writeFile(
+      //@ts-ignore
+        `${message.t}.${mime.extension(message.mimetype)}`,
+        mediaData,
+        function(err) {
+          if (err) {
+            return console.log(err);
+          }
+          console.log('The file was saved!');
+        }
+      );
+    }
   });
 }
 
