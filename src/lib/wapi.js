d14a523d0042130ea19915536d1b060fe07db6f1
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2019-12-17 17:35:16 +0300

    init send payment. DOESNT WORK YET, MAYBE WORKS IN INDIA + US

--- src/lib/wapi.js
+++ src/lib/wapi.js
@@ -1312,6 +1312,40 @@ window.WAPI.sendLocation = async function (chatId, lat, lng, loc) {
 
 
 /**
+ * Send Payment Request
+ *
+ * @param {string} chatId '000000000000@c.us'
+ * @param {string} amount1000 The amount in base value / 10 (e.g 50000 in GBP = £50)
+ * @param {string} currency Three letter currency code (e.g SAR, GBP, USD, INR, AED, EUR)
+ * @param {string} note message to send with the payment request
+ */
+window.WAPI.sendPaymentRequest = async function (chatId, amount1000, currency, noteMessage) {
+    var chat = Store.Chat.get(chatId);
+    var tempMsg = Object.create(chat.msgs.filter(msg => msg.__x_isSentByMe)[0]);
+    var newId = window.WAPI.getNewMessageId(chatId);
+    var extend = {
+        ack: 0,
+        id: newId,
+        local: !0,
+        self: "out",
+        t: parseInt(new Date().getTime() / 1000),
+        to: chatId,
+        isNewMsg: !0,
+        type: "payment",
+        subtype: "request",
+        amount1000,
+        requestFrom: chatId,
+        currency,
+        noteMessage,
+        expiryTimestamp: parseInt(new Date(new Date().setDate(new Date().getDate() + 1)).getTime() / 1000)
+    };
+    Object.assign(tempMsg, extend);
+    await Store.beta.addAndSendMsgToChat(chat, tempMsg)
+};
+
+
+
+/**
  * Send Customized VCard without the necessity of contact be a Whatsapp Contact
  *
  * @param {string} chatId '000000000000@c.us'
