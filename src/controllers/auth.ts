89d5ab9150b21e0461922e2fb1bc87ac28cd8550
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-01-28 14:59:30 +0000

    clean auth.ts

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -45,9 +45,7 @@ export const isInsideChat = (waPage: puppeteer.Page) => {
 export async function retrieveQR(waPage: puppeteer.Page, sessionId?:string) {
   await waPage.evaluate(() => {
     //@ts-ignore
-    if(!window.K) {
-      if(webpackJsonp) window.K = webpackJsonp([], null, ["eaaehfdjdg"]); else {console.log('Seems as though you have been TOS_BLOCKed, unable to refresh QR Code. Please see https://github.com/smashah/sulla#best-practice for information on how to prevent this from happeing. You will most likely not get a QR Code');return;};
-    }
+    if(!window.K) {if(webpackJsonp) window.K = webpackJsonp([], null, ["eaaehfdjdg"]); else {console.log('Seems as though you have been TOS_BLOCKed, unable to refresh QR Code. Please see https://github.com/smashah/sulla#best-practice for information on how to prevent this from happeing. You will most likely not get a QR Code');return;};}
     //@ts-ignore
     window.K.default.state="UNPAIRED"
     //@ts-ignore
