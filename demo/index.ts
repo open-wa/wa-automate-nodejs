d0837473da9bc8885afd9c5489740cd6e19c55be
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-02-04 16:09:08 +0000

    updated smartUserAgent

--- demo/index.ts
+++ demo/index.ts
@@ -1,7 +1,7 @@
 // const sulla = require('../dist/index');
 // var create = require("sulla").create;
 // import { create, Whatsapp, decryptMedia, ev } from '../dist/index';
-import { create, Whatsapp, decryptMedia, ev } from '../src/index';
+import { create, Whatsapp, decryptMedia, ev, smartUserAgent } from '../src/index';
 const mime = require('mime-types');
 const fs = require('fs');
 const uaOverride = 'WhatsApp/2.16.352 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Safari/605.1.15';
--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,5 +1,9 @@
-export const smartUserAgent = (useragent:string) => {
-    const v = '0.4.315';
+/**
+ *  Use this to generate a more likely valid user agent. It makes sure it has the Whatsapp part and replaces any windows or linux os info with mac.
+ * @param useragent Your custom user agent
+ * @param v The whatsapp version from the debug info. This is optional. default is 0.4.315
+ */
+export const smartUserAgent = (useragent:string, v:string= '0.4.315') => {
     useragent = useragent.replace(useragent.match(/\(([^()]*)\)/g).find(x=>x.toLowerCase().includes('linux')||x.toLowerCase().includes('windows')),'(Macintosh; Intel Mac OS X 10_15_2)')
     if(!useragent.includes('WhatsApp')) return `WhatsApp/${v} ${useragent}`;
     return useragent.replace(useragent.match(/WhatsApp\/([.\d])*/g)[0].match(/[.\d]*/g).find(x=>x),v);
