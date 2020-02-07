82ee145b9d83222aa506ff3bfc72a9aacd9164da
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-02-07 04:35:08 +0000

    construct buttons but cant send #98

--- src/lib/wapi.js
+++ src/lib/wapi.js
@@ -27,11 +27,13 @@ if (!window.Store) {
                 { id: "SendSeen", conditions: (module) => (module.sendSeen) ? module.sendSeen : null },
                 { id: "sendDelete", conditions: (module) => (module.sendDelete) ? module.sendDelete : null },
                 { id: "addAndSendMsgToChat", conditions: (module) => (module.addAndSendMsgToChat) ? module.addAndSendMsgToChat : null },
+                { id: "sendMsgToChat", conditions: (module) => (module.sendMsgToChat) ? module.sendMsgToChat : null },
                 { id: "Catalog", conditions: (module) => (module.Catalog) ? module.Catalog : null },
                 { id: "Parser", conditions: (module) => (module.convertToTextWithoutSpecialEmojis) ? module.default : null },
                 { id: "Builders", conditions: (module) => (module.TemplateMessage && module.HydratedFourRowTemplate) ? module : null },
                 { id: "Identity", conditions: (module) => (module.queryIdentity && module.updateIdentity) ? module : null },
                 { id: "Features", conditions: (module) => (module.FEATURE_CHANGE_EVENT && module.features) ? module : null },
+                { id: "MessageUtils", conditions: (module) => (module.storeMessages && module.appendMessage) ? module : null },
                 { id: "Participants", conditions: (module) => (module.addParticipants && module.removeParticipants && module.promoteParticipants && module.demoteParticipants) ? module : null }
             ];
             for (let idx in modules) {
@@ -86,6 +88,7 @@ if (!window.Store) {
 // window.Store.d = webpackJsonp([], null, ["dbdfbgehgj"]);
 // window.Store.e = webpackJsonp([], null, ["bbchdeehff"]);
 // window.Store.buttons = webpackJsonp([], null, ["cdaaeifjfh"]);
+window.Store.sendMsgRecord=webpackJsonp([],null,['cjafhagbj']).sendMsgRecord;
 
 window.WAPI = {
     lastRead: {}
@@ -1688,8 +1691,10 @@ hydratedContentText:'hellllloooowww',
 
 window.WAPI.sendButtons2 = async function(chatId){
     var chat = Store.Chat.get(chatId);
-    var tempMsg = Object.create(chat.msgs.filter(msg => msg.__x_isSentByMe)[0]);
+    var tempMsg = Object.create(chat.msgs.filter(msg => msg.__x_isSentByMe && msg.type=="chat")[0]);
+    var t2 = Object.create(chat.msgs.filter(msg => msg.__x_isSentByMe)[0]);
     var newId = window.WAPI.getNewMessageId(chatId);
+    delete tempMsg.hasTemplateButtons;
     var extend = {
         ack: 0,
         id: newId,
@@ -1705,8 +1710,9 @@ window.WAPI.sendButtons2 = async function(chatId){
         broadcast:false,
         isQuotedMsgAvailable:true,
         shouldEnableHsm:true,
-        __x_hasTemplateButtons:false,
-        invis:true
+        __x_hasTemplateButtons:true,
+        invis:true,
+        body: 'body text'
     };
 
     Object.assign(tempMsg, extend);
@@ -1717,20 +1723,42 @@ hydratedButtons:[
     {callButton:new Store.Builders.HydratedCallButton({displayText:'test call',phoneNumber:"4477777777777"})},
     {urlButton:new Store.Builders.HydratedURLButton({displayText:'test url',url:"https://google.com"})}
 ],
-hydratedContentText:'hellllloooowww'
+hydratedContentText:'hellllloooowww',
+hydratedFooterText:"asdasd",
+hydratedTitleText:"asdasd232"
 });
 
-
-    Store.Parser.parseTemplateMessage(tempMsg,btns);
-    // tempMsg._minEphemeralExpirationTimestamp()
+    Store.Parser.parseTemplateMessage(t2,btns);
+    tempMsg.buttons=t2.buttons;
+    tempMsg.mediaData = {};
+    tempMsg._minEphemeralExpirationTimestamp()
     // tempMsg.senderObj.isBusiness=true;
     // tempMsg.senderObj.isEnterprise=true;
-    await Store.addAndSendMsgToChat(chat, tempMsg)
-
+    tempMsg.senderObj = {
+      ...tempMsg.senderObj,
+      isBusiness:true,
+      isEnterprise:true,
+      notifyName:"pow.bike",
+      mentionName:"Button Test",
+      displayName:"Button Test",
+      searchName:"button test",
+      header:'b',
+      formattedShortNameWithNonBreakingSpaces:"Button test",
+      formattedShortName:"Button test",
+      formattedName:"Button test",
+      formattedUser:"Button test",
+      
+    }
+    await Store.MessageUtils.appendMessage(chat,tempMsg)
+    var t = Store.Msg.add(tempMsg)[0]
+    await chat.sendQueue.enqueue(chat.addQueue.enqueue(t.waitForPrep().then(_=>t)).then(t=>chat.msgs.add(t)).catch(e=>console.log(e))).then(e => {
+        var t = e[0];
+        console.log('t',t)
+        //comment the next line to see the buttons. the next line will send the message but whatsapp wil sanitize it of all the buttony goodness
+        return Store.sendMsgRecord(t)
+        // return Store.WapQuery.msgCreateRecord(tempMsg)
+    })
 }
-
-
-
 
 window.WAPI.reply = async function (chatId, body, quotedMsg) {
     if (typeof quotedMsg !== "object") quotedMsg = Store.Msg.get(quotedMsg)
