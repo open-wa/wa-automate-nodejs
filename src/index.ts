f628eef9dc4220f2a199a09df4e17ad33243c0a0
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-04-19 13:56:05 +0100

    migrated decrypt to `@open-wa/wa-decrypt` #334

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { Chat, Contact, Message } from './api/model';
 export { Whatsapp } from './api/whatsapp';
 export { create } from './controllers/initializer';
-export { decryptMedia } from './utils/decrypt';
+export * from '@open-wa/wa-decrypt';
 export { ev } from './controllers/events'
 export { smartUserAgent } from './utils/tools'
