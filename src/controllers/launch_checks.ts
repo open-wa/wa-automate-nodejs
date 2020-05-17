08894869793bc700edba3352f6424ab37766598a
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-05-17 17:29:40 +0100

    fix: added successfull repair spinner condition

--- src/controllers/launch_checks.ts
+++ src/controllers/launch_checks.ts
@@ -40,7 +40,7 @@ export async function integrityCheck(waPage, notifier, spinner, debugInfo) {
       }
         spinner.fail(`Unable to repair broken methods. Sometimes this happens the first time after a new WA version, please try again. Please check the issues in the repo for updates: https://github.com/open-wa/wa-automate-nodejs/issues`);
         }
-    }
+    } else spinner.succeed('Session repaired.');
   } else spinner.succeed('Passed Integrity Test');
   return true;
 }
