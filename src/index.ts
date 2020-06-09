c6ba57649e44aeab73ea1e1590be08d2672724b1
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-06-09 08:04:15 +0100

    Expose AvailableWebhooks enum

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Chat, Contact, Message } from './api/model';
-export { Client } from './api/Client';
+export { Client, AvailableWebhooks } from './api/Client';
 export { create } from './controllers/initializer';
 export * from '@open-wa/wa-decrypt';
 export { ev } from './controllers/events'
