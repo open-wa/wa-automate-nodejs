ccbf1d2bdc7745f5cffa2031ad5dcb59406e4281
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-07-16 03:19:02 +0100

    chore: update supported version in readme

--- src/config/puppeteer.config.ts
+++ src/config/puppeteer.config.ts
@@ -47,7 +47,7 @@ const puppeteerConfig = {
   ]
 };
 
-export const useragent = 'WhatsApp/2.2027.10 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36';
+export const useragent = 'WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36';
 export const createUserAgent = (waVersion:string) => `WhatsApp/${waVersion} Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36`;
 export { puppeteerConfig };
 
