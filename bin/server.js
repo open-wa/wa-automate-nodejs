13adf9cd14412f03e20f61964b7cb71468a69579
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-07-16 04:39:04 +0100

    _

--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,9 @@
 #! /usr/bin/env node
 
-// require("ts-node").register({
-// 	"ignore": [/\.js/],
-// 	//"typeRoots": ["./node_modules/@types"],
-// 	// ...require("./tsconfig.json")	
-// });
+require("ts-node").register({
+	"ignore": [/\.js/],
+	//"typeRoots": ["./node_modules/@types"],
+	// ...require("./tsconfig.json")	
+});
 
-require('../dist/bin');
+require('./index.ts');
