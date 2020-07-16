53660d55d7cd0aa806f445f419115a86955386fe
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-07-16 03:11:30 +0100

    Added better error handling on sendText #645

--- src/api/Client.ts
+++ src/api/Client.ts
@@ -740,13 +740,22 @@ public async onLiveLocation(chatId: ChatId, fn: (liveLocationChangedEvent: LiveL
    * @param content text message
    */
   public async sendText(to: ChatId, content: Content) {
-    return await this.pup(
+   const err = [
+    'Not able to send message to broadcast',
+    'Not a contact',
+    'Error: Number not linked to WhatsApp Account',
+    'ERROR: Please make sure you have at least one chat'
+   ];
+
+    let res = await this.pup(
       ({ to, content }) => {
         WAPI.sendSeen(to);
         return WAPI.sendMessage(to, content);
       },
       { to, content }
     );
+    if(err.includes(res)) console.error(res);
+    return err.includes(res) ? false : res;
   }
   
 
