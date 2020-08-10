e2fe8c0c868d824977ac62e4d6a74d1686f7c89a
Mohammed Shah <shah@idk.uno>
2020-08-10 21:47:14 +0100

    added sendContact patch (#706)

--- demo/index.ts
+++ demo/index.ts
@@ -182,7 +182,7 @@ create({
   killProcessOnBrowserClose: true,
   autoRefresh:true, //default to true
   qrRefreshS:15, //please note that if this is too long then your qr code scan may end up being invalid. Generally qr codes expire every 15 seconds.
-  safeMode: true
+  safeMode: true,
   // cacheEnabled:false,
   // devtools:true,
   //OR
