089dc3574ff4aff52d36974e0a1a59df34f24ef9
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-09-17 09:03:34 +0100

    updated wapi md5 for launch check

--- src/controllers/launch_checks.ts
+++ src/controllers/launch_checks.ts
@@ -4,7 +4,7 @@ var uniq = require('lodash.uniq');
 const fs = require('fs');
 var pkg = require('../../package.json');
 const hasha = require('hasha');
-const currentHash = '78c86cb0977d3b0d595d1e8e062bc4ac'
+const currentHash = 'f1d7de3a7817b194743782383fd7b0f9'
 
 export async function checkWAPIHash(){
   const h =  await hasha.fromFile(path.join(__dirname, '../lib', 'wapi.js'), {algorithm: 'md5'});
