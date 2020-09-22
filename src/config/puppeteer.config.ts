c8c4b1ac1b96c9b04568559b0fda65a5a1f1add9
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-09-22 13:33:47 +0100

    Update user agent

--- src/config/puppeteer.config.ts
+++ src/config/puppeteer.config.ts
@@ -46,7 +46,7 @@ const puppeteerConfig = {
   ]
 };
 
-export const useragent = 'WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36';
+export const useragent = 'WhatsApp/2.2037.6 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36';
 export const createUserAgent = (waVersion:string) => `WhatsApp/${waVersion} Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36`;
 export { puppeteerConfig };
 
