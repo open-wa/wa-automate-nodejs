7070c9353a7155bf8f94adfca9ee61f01c5420c9
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-10-16 10:47:01 +0100

    fix: #943

--- src/controllers/browser.ts
+++ src/controllers/browser.ts
@@ -21,8 +21,8 @@ export async function initClient(sessionId?: string, config?:ConfigObject, custo
   }
   await waPage.setUserAgent(customUserAgent||useragent);
   await waPage.setViewport({
-    width: config?.window?.width || width,
-    height: config?.window?.height || height,
+    width: config?.viewport?.width || width,
+    height: config?.viewport?.height || height,
     deviceScaleFactor: 1
   });
   const cacheEnabled = config?.cacheEnabled === false ? false : true;
