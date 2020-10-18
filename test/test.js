54c2712d68623dca77d443b8ddf2d5093fd5a4a6
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-10-18 08:41:20 +0100

    Added some more pseudo tests

--- test/test.js
+++ test/test.js
@@ -4,6 +4,17 @@ import test from 'ava';
  test.todo('Create a client with 1 params [session id string]')
  test.todo('Create a client with 1 params [config only]')
  test.todo('Create a client with 2 params')
+
+ /**
+  * Logout states
+  */
+ test.todo('Restart existing session')
+ test.todo('Logout and detect logout event')
+ test.todo('Restart existing session, expect QR code')
+ test.todo('create fresh session, expect QR code')
+ test.todo('create fresh session, wait 60 seconds, expect QR code')
+
+
  test.todo('Check if can send message')
  test.todo('Check licensed features')
  test.todo('Create a client with license')
@@ -29,4 +40,5 @@ test.todo('Send Image')
 test.todo('Send Video')
 test.todo('Send Location')
 test.todo('Send Sticker')
+test.todo('Send Instagram link preview')
 test.todo('etc..')
