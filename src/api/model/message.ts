d6867ac7238793b05ed269474409be385e92a397
M SHAH <shah@idk.uno>
2021-02-18 11:51:26 +0000

    Revert "improved message and chat typings"
    This reverts commit 70abe098e1f6092b420dec665562f48c8f2290b6.

--- src/api/model/message.ts
+++ src/api/model/message.ts
@@ -7,15 +7,15 @@ export interface Message {
   /**
    * The id of the message
    */
-  id?: MessageId;
+  id: MessageId;
   /**
    * The body of the message. If the message type is `chat` , `body` will be the text of the chat. If the message type is some sort of media, then this body will be the thumbnail of the media.
    */
-  body?: string;
+  body: string;
   /**
    * The type of the message, see [[MessageTypes]]
    */
-  type?: MessageTypes;
+  type: MessageTypes;
   mimetype?: string;
   /**
    * The latitude of a location message
@@ -32,86 +32,110 @@ export interface Message {
   /**
    * The timestamp of the message
    */
-  t?: number;
-  notifyName?: string;
+  t: number;
+  notifyName: string;
   /**
    * The chat from which the message was sent
    */
-  from?: ChatId;
+  from: ChatId;
   /**
    * The chat id to which the message is being sent
    */
-  to?: ChatId;
+  to: ChatId;
   /**
    * Indicates whether the message was sent by the host account
    */
-  self?: boolean;
+  self: boolean;
   /**
    * The length of the media in the message, if it exists.
    */
-  duration?: string | number;
+  duration?: string|number;
   /**
    * The acknolwedgement state of a message [[MessageAck]]
    */
-  ack?: MessageAck;
-  invis?: boolean;
-  isNewMsg?: boolean;
-  star?: boolean;
-  recvFresh?: boolean;
+  ack: MessageAck;
+  invis: boolean;
+  isNewMsg: boolean;
+  star: boolean;
+  recvFresh: boolean;
   /**
    * If the message is sent as a broadcast
    */
-  broadcast?: boolean;
+  broadcast: boolean;
   /**
    * If the message has been forwarded
    */
-  isForwarded?: boolean;
+  isForwarded: boolean;
   /**
    * The labels associated with the message (used with business accounts)
    */
-  labels?: string[];
+  labels: string[];
   /**
    * An array of all mentioned numbers in this message.
    */
-  mentionedJidList ?: string[];
+  mentionedJidList: string[];
   /**
    * If the message is of a media type, it may also have a caption
    */
-  caption?: string;
+  caption: string;
   /**
    * The contact object of the account that sent the message
    */
-  sender?: Contact;
+  sender: Contact;
   /**
    * the timestanmp of the message
    */
-  timestamp?: number;
-  content?: string;
-  isGroupMsg?: boolean;
-  isMMS?: boolean;
-  isMedia?: boolean;
-  isNotification?: boolean;
-  isPSA?: boolean;
+  timestamp: number;
+  content: string;
+  isGroupMsg: boolean;
+  isMMS: boolean;
+  isMedia: boolean;
+  isNotification: boolean;
+  isPSA: boolean;
   /**
    * If the message is from the host account
    */
-  fromMe?: boolean;
+  fromMe: boolean;
   /**
    * The chat object
    */
-  chat?: Chat;
-  chatId?: ChatId;
-  author?: string;
+  chat: {
+    id: ChatId;
+    pendingMsgs: boolean;
+    lastReceivedKey: {
+      fromMe: boolean;
+      remote: ChatId;
+      id: MessageId;
+      _serialized: MessageId;
+    };
+    t: number;
+    unreadCount: number;
+    archive: boolean;
+    isReadOnly: boolean;
+    modifyTag: number;
+    muteExpiration: number;
+    name: string;
+    notSpam: boolean;
+    pin: number;
+    msgs: Message[];
+    kind: string;
+    isGroup: boolean;
+    contact: Chat;
+    groupMetadata: GroupMetadata;
+    presence: { id: ContactId; chatstates: any[] };
+  };
+  chatId: ChatId;
+  author: string;
   /**
    * @deprecated
    */
-  clientUrl?: string;
-  deprecatedMms3Url?: string;
-  quotedMsg?: Message;
-  quotedMsgObj?: Message;
-  mediaData?: {};
-  shareDuration?: number;
-  isAnimated?: boolean;
+  clientUrl: string;
+  deprecatedMms3Url: string;
+  quotedMsg: Message;
+  quotedMsgObj: Message;
+  mediaData: {};
+  shareDuration: number;
+  isAnimated: boolean;
 }
 
 
