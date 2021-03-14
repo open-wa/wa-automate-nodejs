a55ee43903611d5e72b92bff73fd9a51c0cb60e8
M SHAH <shah@idk.uno>
2021-03-14 17:17:19 +0000

    ✨  added `square` in `Mp4StickerConversionProcessOptions` to set sticker output dimention #1474

--- src/api/model/media.ts
+++ src/api/model/media.ts
@@ -29,6 +29,10 @@ export type StickerMetadata = {
      * Setting this to `true` will skip the resizing/square-cropping of the sticker. It will instead 'letterbox' the image with a transparent background.
      */
     keepScale ?: boolean
+    /**
+     * Your Discord ID to get onto the sticker leaderboard!
+     */
+    discord ?: string
   }
 
   export type Mp4StickerConversionProcessOptions = {
@@ -62,6 +66,11 @@ export type StickerMetadata = {
      * @default `false`
      */
     log?: boolean
+    /**
+     * A number representing the WxH of the output sticker (default `512x512`). Lowering this number is a great way to process longer duration stickers. The max value is `512`.
+     * default `512`
+     */
+    square ?: number
   }
 
   export const defaultProcessOptions : Mp4StickerConversionProcessOptions = {
@@ -70,5 +79,6 @@ export type StickerMetadata = {
     endTime :  `00:00:05.0`,
     loop: 0,
     crop: true,
-    log: false
+    log: false,
+    square: 512
   }
