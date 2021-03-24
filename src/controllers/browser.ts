88ce46fa46c47a37fd0fea73473419fbbd726770
M SHAH <shah@idk.uno>
2021-03-24 14:34:30 +0000

    ⚡️ launch time improvements  #1514

--- src/controllers/browser.ts
+++ src/controllers/browser.ts
@@ -12,25 +12,26 @@ import { ConfigObject } from '../api/model';
 const ON_DEATH = require('death'); //this is intentionally ugly
 let browser;
 
-export async function initClient(sessionId?: string, config?:ConfigObject, customUserAgent?:string) {
+export async function initPage(sessionId?: string, config?:ConfigObject, customUserAgent?:string) : Promise<Page> {
+  const setupPromises = [];
   if(config?.useStealth) puppeteer.use(require('puppeteer-extra-plugin-stealth')());
   browser = await initBrowser(sessionId,config);
   const waPage = await getWAPage(browser);
   if (config?.proxyServerCredentials) {
     await waPage.authenticate(config.proxyServerCredentials);
   }
-  await waPage.setUserAgent(customUserAgent||useragent);
+  setupPromises.push(waPage.setUserAgent(customUserAgent||useragent));
   if(config?.defaultViewport!==null)
-  await waPage.setViewport({
+  setupPromises.push(waPage.setViewport({
     width: config?.viewport?.width || width,
     height: config?.viewport?.height || height,
     deviceScaleFactor: 1
-  });
+  }));
   if(config?.resizable) config.defaultViewport= null
   const cacheEnabled = config?.cacheEnabled === false ? false : true;
   const blockCrashLogs = config?.blockCrashLogs === false ? false : true;
-  await waPage.setBypassCSP(config?.bypassCSP || false);
-  await waPage.setCacheEnabled(cacheEnabled);
+  setupPromises.push(waPage.setBypassCSP(config?.bypassCSP || false));
+  setupPromises.push(waPage.setCacheEnabled(cacheEnabled));
   const blockAssets = !config?.headless ? false : config?.blockAssets || false;
   if(blockAssets){
     puppeteer.use(require('puppeteer-extra-plugin-block-resources')({
@@ -51,33 +52,35 @@ export async function initClient(sessionId?: string, config?:ConfigObject, custo
     .replace('://', '')}` : config.proxyServerCredentials.address}` : false;
   let quickAuthed = false;
   if(interceptAuthentication || proxyAddr || blockCrashLogs){
-    await waPage.setRequestInterception(true);  
-    let authCompleteEv = new EvEmitter(sessionId, 'AUTH');
-    waPage.on('request', async request => {
-      if (
-        interceptAuthentication &&
-        request.url().includes('_priority_components') &&
-        !quickAuthed
-      ) {
-        authCompleteEv.emit(true);
-        await waPage.evaluate('window.WA_AUTHENTICATED=true;');
-        quickAuthed = true;
+    setupPromises.push(async ()=>{
+      await waPage.setRequestInterception(true);  
+      const authCompleteEv = new EvEmitter(sessionId, 'AUTH');
+      waPage.on('request', async request => {
+        if (
+          interceptAuthentication &&
+          request.url().includes('_priority_components') &&
+          !quickAuthed
+        ) {
+          authCompleteEv.emit(true);
+          await waPage.evaluate('window.WA_AUTHENTICATED=true;');
+          quickAuthed = true;
+        }
+      if (request.url().includes('https://crashlogs.whatsapp.net/') && blockCrashLogs){
+        request.abort();
       }
-    if (request.url().includes('https://crashlogs.whatsapp.net/') && blockCrashLogs){
-      request.abort();
-    }
-    else if (proxyAddr) require('puppeteer-page-proxy')(request, proxyAddr);
-    else request.continue();
+      else if (proxyAddr) require('puppeteer-page-proxy')(request, proxyAddr);
+      else request.continue();
+      })
     })
   }
 
   //check if [session].json exists in __dirname
   const sessionjsonpath = (config?.sessionDataPath && config?.sessionDataPath.includes('.data.json')) ? path.join(path.resolve(process.cwd(),config?.sessionDataPath || '')) : path.join(path.resolve(process.cwd(),config?.sessionDataPath || ''), `${sessionId || 'session'}.data.json`);
   let sessionjson = '';
-  let sd = process.env[`${sessionId.toUpperCase()}_DATA_JSON`] ? JSON.parse(process.env[`${sessionId.toUpperCase()}_DATA_JSON`]) : config?.sessionData;
+  const sd = process.env[`${sessionId.toUpperCase()}_DATA_JSON`] ? JSON.parse(process.env[`${sessionId.toUpperCase()}_DATA_JSON`]) : config?.sessionData;
   sessionjson = (typeof sd === 'string') ? JSON.parse(Buffer.from(sd, 'base64').toString('ascii')) : sd;
   if (fs.existsSync(sessionjsonpath)) {
-    let s = fs.readFileSync(sessionjsonpath, "utf8");
+    const s = fs.readFileSync(sessionjsonpath, "utf8");
     try {
       sessionjson = JSON.parse(s);
     } catch (error) {
@@ -88,7 +91,7 @@ export async function initClient(sessionId?: string, config?:ConfigObject, custo
     if(p) {
       const altSessionJsonPath = (config?.sessionDataPath && config?.sessionDataPath.includes('.data.json')) ? path.join(path.resolve(p,config?.sessionDataPath || '')) : path.join(path.resolve(p,config?.sessionDataPath || ''), `${sessionId || 'session'}.data.json`);
       if(fs.existsSync(altSessionJsonPath)) {
-        let s = fs.readFileSync(altSessionJsonPath, "utf8");
+        const s = fs.readFileSync(altSessionJsonPath, "utf8");
         try {
           sessionjson = JSON.parse(s);
         } catch (error) {
@@ -106,32 +109,27 @@ export async function initClient(sessionId?: string, config?:ConfigObject, custo
       await require('puppeteer-page-proxy')(waPage, proxyAddr);
       console.log(`Active proxy: ${config.proxyServerCredentials.address}`)
     }
+  await Promise.all(setupPromises);
   await waPage.goto(puppeteerConfig.WAUrl)
   return waPage;
 }
 
-export async function injectApi(page: Page) {
-  await page.addScriptTag({
-    path: require.resolve(path.join(__dirname, '../lib', 'wapi.js'))
-  });
-  await page.addScriptTag({
-    path: require.resolve(path.join(__dirname, '../lib', 'axios.min.js'))
-  });
-  await page.addScriptTag({
-    path: require.resolve(path.join(__dirname, '../lib', 'jsSha.min.js'))
-  });
-  await page.addScriptTag({
-    path: require.resolve(path.join(__dirname, '../lib', 'base64.js'))
-  });
-  await page.addScriptTag({
-    path: require.resolve(path.join(__dirname, '../lib', 'qr.min.js'))
-  });
-  await page.addScriptTag({
-    path: require.resolve(path.join(__dirname, '../lib', 'hash.js'))
-  });
-  await page.addScriptTag({
-    path: require.resolve(path.join(__dirname, '../lib', 'launch.js'))
-  });
+export const addScript = (page: Page, js : string) : Promise<unknown> => page.addScriptTag({
+  path: require.resolve(path.join(__dirname, '../lib', js))
+})
+
+export async function injectApi(page: Page) : Promise<Page> {
+await Promise.all(
+  [
+    'axios.min.js',
+    'jsSha.min.js',
+    'qr.min.js',
+    'base64.js',
+    'hash.js'
+  ].map(js=>addScript(page,js))
+  );
+  await addScript(page,'wapi.js')
+  await addScript(page,'launch.js')
   return page;
 }
 
@@ -139,7 +137,7 @@ async function initBrowser(sessionId?: string, config:any={}) {
   if(config?.useChrome && !config?.executablePath) {
     const storage = require('node-persist');
     await storage.init();
-    let _savedPath = await storage.getItem('executablePath');
+    const _savedPath = await storage.getItem('executablePath');
     if(!_savedPath) {
       config.executablePath = require('chrome-launcher').Launcher.getInstallations()[0];
       await storage.setItem('executablePath',config.executablePath)
@@ -166,7 +164,7 @@ async function initBrowser(sessionId?: string, config:any={}) {
   
   // if(config?.proxyServerCredentials?.address) puppeteerConfig.chromiumArgs.push(`--proxy-server=${config.proxyServerCredentials.address}`)
   if(config?.browserWsEndpoint) config.browserWSEndpoint = config.browserWsEndpoint;
-  let args = [...puppeteerConfig.chromiumArgs,...(config?.chromiumArgs||[])];
+  const args = [...puppeteerConfig.chromiumArgs,...(config?.chromiumArgs||[])];
   if(config?.corsFix) args.push('--disable-web-security');
   const browser = (config?.browserWSEndpoint) ? await puppeteer.connect({...config}): await puppeteer.launch({
     headless: true,
@@ -195,7 +193,7 @@ async function getWAPage(browser: Browser) {
   return pages[0];
 }
 
-ON_DEATH(async (signal, err) => {
+ON_DEATH(async () => {
   //clean up code here
   if (browser) await browser.close();
 });
