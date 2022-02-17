130713b223161845d97178eba0c28c4b13630f8a
M SHAH <shah@idk.uno>
2022-02-17 13:29:21 +0300

    ⚡️ improved injection checker

--- src/controllers/patch_manager.ts
+++ src/controllers/patch_manager.ts
@@ -132,7 +132,9 @@ export async function getLicense(config: ConfigObject, me: {
 
 export async function earlyInjectionCheck(page: Page): Promise<(page: Page) => boolean> {
   //@ts-ignore
-  return await page.evaluate(() => { if (window.webpackChunkwhatsapp_web_client) { window.webpackChunkbuild = window.webpackChunkwhatsapp_web_client; } else { (function () { const f = Object.entries(window).filter(([, o]) => o && o.push && (o.push != [].push)); if (f[0]) { window.webpackChunkbuild = window[f[0][0]]; } })(); } return (typeof webpackChunkbuild !== "undefined"); });
+  await page.waitForFunction(()=>Object.entries(window).filter(([, o]) => o && o.push && (o.push != [].push))[0] ? true : false, { timeout: 10, polling: 500 }).catch(()=>{})
+  //@ts-ignore
+  return await page.evaluate(() => { if (window.webpackChunkwhatsapp_web_client) { window.webpackChunkbuild = window.webpackChunkwhatsapp_web_client; } else { (function () { const f = Object.entries(window).filter(([, o]) => o && o.push && (o.push != [].push)); if (f[0]) { window.webpackChunkbuild = window[f[0][0]]; } })(); } return (typeof window.webpackChunkbuild !== "undefined"); });
 }
 
 export async function getAndInjectLicense(page: Page, config: ConfigObject, me: {
