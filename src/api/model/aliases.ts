631900654448fe0683323b0184475c997feca8c8
M SHAH <shah@idk.uno>
2022-03-17 17:38:44 +0000

    🏷️  types: update `GroupChatId` to include new version #2575

--- src/api/model/aliases.ts
+++ src/api/model/aliases.ts
@@ -34,7 +34,7 @@ export type AccountNumber = `${number}`;
  * 
  * `"447123456789-1445627445@g.us"`
  */
-export type GroupChatId = `${AccountNumber}-${number}@${GroupChatServer}`;
+export type GroupChatId = `${AccountNumber}-${number}@${GroupChatServer}` | `${number}@${GroupChatServer}`;
 
 /**
  * A contact id ends with `@c.us` and only contains the number of the contact. For example, if the country code of a contact is `44` and their number is `7123456789` then the contact id would be `447123456789@c.us`
