2f584fbc2ee84d42df85ec2776ea5d80e116830c
M SHAH <shah@idk.uno>
2022-10-13 04:12:18 +0100

    Deploy website - based on 5e294cffc442a5c60db5bb3fbfd0b9aa81c963cb

--- js/gumroad_docusaurus_cart_fix.js
+++ js/gumroad_docusaurus_cart_fix.js
@@ -1,23 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    (new MutationObserver((mutationList, observer) => {
-        for (const mutation of mutationList) {
-            if(mutation.type === "childList" && mutation.addedNodes && [...mutation.addedNodes].find(node=>node.className==="gumroad")) { 
-              console.log('Gattem', mutation);
-                const grNode = [...mutation.addedNodes].find(node=>node.className==="gumroad")
-                const cartbtn = grNode.getElementsByClassName('cart-button')[0]
-                grNode.style.position = cartbtn.style.position = ''
-                grNode.style.padding = '4px'
-                const isMobile = !!document.getElementsByClassName('menu__list').length
-                const addTo = document.getElementsByClassName('menu__list')[0] || document.getElementsByClassName("navbar__items navbar__items--right")[0]
-                if(isMobile) {
-                    addTo.appendChild(grNode);
-                    grNode.style.padding = '16px'
-                    grNode.style.width = cartbtn.style.width = '-webkit-fill-available'
+    if(!window.grObserver) {
+        window.grObserver = new MutationObserver((mutationList, observer) => {
+            for (const mutation of mutationList) {
+                if(mutation.type === "childList" && mutation.addedNodes && [...mutation.addedNodes].find(node=>node.className==="gumroad")) { 
+                  console.log('Gattem', mutation);
+                    const grNode = [...mutation.addedNodes].find(node=>node.className==="gumroad")
+                    const cartbtn = grNode.getElementsByClassName('cart-button')[0]
+                    grNode.style.position = cartbtn.style.position = ''
+                    grNode.style.padding = '4px'
+                    const isMobile = !!document.getElementsByClassName('menu__list').length
+                    const addTo = document.getElementsByClassName('menu__list')[0] || document.getElementsByClassName("navbar__items navbar__items--right")[0]
+                    if(isMobile) {
+                        addTo.appendChild(grNode);
+                        grNode.style.padding = '16px'
+                        grNode.style.width = cartbtn.style.width = '-webkit-fill-available'
+                    }
+                    else addTo.insertBefore(grNode,document.getElementsByClassName('DocSearch DocSearch-Button')[0].parentElement)
                 }
-                else addTo.insertBefore(grNode,document.getElementsByClassName('DocSearch DocSearch-Button')[0].parentElement)
             }
-        }
-    })).observe(document.body, {childList: true, subtree: true });
+        })
+        window.grObserver.observe(document.body, {childList: true, subtree: true });
+    }
  });
 
 
