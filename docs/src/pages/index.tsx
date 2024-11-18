e0ac00f2b67bc192741f00b431e0d6fffa4a2095
M SHAH <shah@idk.uno>
2024-11-18 13:33:19 +0000

    update docs button

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/reference/api/Client/classes/Client">
+            See all Features ⏩
           </Link>
         </div>
       </div>
