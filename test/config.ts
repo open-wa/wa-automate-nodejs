ceae9143986395f0051b4e1a5043c3840c14de2f
M SHAH <shah@idk.uno>
2025-12-07 04:19:28 +0000

    Squashed 'packages/socket-client/' content from commit c3dc5c344
    git-subtree-dir: packages/socket-client
    git-subtree-split: c3dc5c344f58dbd1966681a94cc08b914e2599bf

---
+++ test/config.ts
@@ -0,0 +1,6 @@
+// __tests__/config.ts
+export const config = {
+    API_URL: "http://localhost:8060", // Replace with your open-wa API URL
+    API_KEY: "your-api-key", // Replace with your open-wa API key, if needed
+    TEST_NUM: "44712345679@c.us"
+  };
